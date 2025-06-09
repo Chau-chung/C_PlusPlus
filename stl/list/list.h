96561852bc6ebbbbf5000a37813e0a366a638fd2
cchung-Uka <chauchung0222@gmail.com>
2025-06-09 18:37:06 +0800

    模拟实现list类

---
+++ stl/list/list.h
@@ -0,0 +1,272 @@
+﻿#pragma once
+
+#include <assert.h>
+#include <algorithm>
+#include "ReverseIterator.h"
+
+namespace sl
+{
+	template<class T>
+	struct ListNode
+	{
+		ListNode<T>* _next;
+		ListNode<T>* _prev;
+		T _data;
+
+		ListNode(const T& x = T())
+			:_next(nullptr)
+			,_prev(nullptr)
+			,_data(x)
+		{}
+	};
+
+	template<class T, class Ref, class Ptr>
+	struct ListIterator
+	{
+		typedef ListNode<T> Node;
+		typedef ListIterator<T, Ref, Ptr> Self;
+
+		Node* _node;
+
+		ListIterator(Node* node)
+			:_node(node)
+		{}
+
+		Ref operator*()
+		{
+			return _node->_data;
+		}
+
+		Ptr operator->()
+		{
+			return &_node->_data;
+		}
+
+		Self& operator++()
+		{
+			_node = _node->_next;
+			return *this;
+		}
+
+		Self operator++(int)
+		{
+			Self tmp(*this);
+			_node = _node->_next;
+
+			return tmp;
+		}
+
+		Self& operator--()
+		{
+			_node = _node->_prev;
+			return *this;
+		}
+
+		Self operator--(int)
+		{
+			Self tmp(*this);
+			_node = _node->_prev;
+
+			return tmp;
+		}
+
+		bool operator!=(const Self& it)
+		{
+			return _node != it._node;
+		}
+
+		bool operator==(const Self& it)
+		{
+			return _node == it._node;
+		}
+	};
+
+	template<class T>
+	class list
+	{
+		typedef ListNode<T> Node;
+	public:
+		typedef ListIterator<T, T&, T*> iterator;
+		typedef ListIterator<T, const T&, const T*> const_iterator;
+
+		typedef ReverseIterator<iterator, T&, T*> reverse_iterator;
+		typedef ReverseIterator<const_iterator, const T&, const T*> const_reverse_iterator;
+
+		iterator begin()
+		{
+			return _head->_next;
+		}
+
+		iterator end()
+		{
+			return _head;
+		}
+
+		const_iterator begin() const
+		{
+			return _head->_next;
+		}
+
+		const_iterator end() const
+		{
+			return _head;
+		}
+
+		reverse_iterator rbegin()
+		{
+			return reverse_iterator(end());
+		}
+
+		reverse_iterator rend()
+		{
+			return reverse_iterator(begin());
+		}
+
+		const_reverse_iterator crbegin() const
+		{
+			return const_reverse_iterator(end());
+		}
+
+		const_reverse_iterator crend() const
+		{
+			return const_reverse_iterator(begin());
+		}
+
+		void empty_init()
+		{
+			_head = new Node;
+			_head->_next = _head;
+			_head->_prev = _head;
+
+			_size = 0;
+		}
+
+		list()
+		{
+			empty_init();
+		}
+		
+		list(initializer_list<T> il)
+		{
+			empty_init();
+
+			for (auto& e : il)
+			{
+				push_back(e);
+			}
+		}
+
+		list(const list<T>& lt)
+		{
+			empty_init();
+
+			for (auto& e : lt)
+			{
+				push_back(e);
+			}
+		}
+
+		void swap(list<T>& lt)
+		{
+			std::swap(_head, lt._head);
+			std::swap(_size, lt._size);
+		}
+
+		list<T>& operator=(list<T> lt)
+		{
+			swap(lt);
+			return *this;
+		}
+
+		void clear()
+		{
+			iterator it = begin();
+			while (it != end())
+			{
+				it = erase(it);
+			}
+		}
+
+		~list()
+		{
+			clear();
+
+			delete _head;
+			_head = nullptr;
+		}
+
+		void push_back(const T& x)
+		{
+			Node* newnode = new Node(x);
+			Node* tail = _head->_prev;
+
+			tail->_next = newnode;
+			newnode->_prev = tail;
+			newnode->_next = _head;
+			_head->_prev = newnode;
+		}
+
+		/*void push_back(const T& x)
+		{
+			insert(end(), x);
+		}*/
+
+		void push_front(const T& x)
+		{
+			insert(begin(), x);
+		}
+
+		void pop_back()
+		{
+			erase(--end());
+		}
+
+		void pop_front()
+		{
+			erase(begin());
+		}
+
+		void insert(iterator pos, const T& val)
+		{
+			Node* cur = pos._node;
+			Node* newnode = new Node(val);
+			Node* prev = cur->_prev;
+
+			prev->_next = newnode;
+			newnode->_prev = prev;
+			newnode->_next = cur;
+			cur->_prev = newnode;
+
+			++_size;
+		}
+
+		iterator erase(iterator pos)
+		{
+			Node* cur = pos._node;
+			Node* prev = cur->_prev;
+			Node* next = cur->_next;
+
+			prev->_next = next;
+			next->_prev = prev;
+
+			delete cur;
+			--_size;
+
+			return next;
+		}
+
+		size_t size()
+		{
+			return _size;
+		}
+
+		bool empty()
+		{
+			return _size == 0;
+		}
+
+	private:
+		Node* _head;
+		size_t _size;
+	};
+}
