b6bfbc01e6f874872798d1f6f94de3e566ef619c
cchung-Uka <zc22045@gmail.com>
2025-06-15 21:47:40 +0800

    模拟实现priority_queue类

--- stl/queue/queue.h
+++ stl/queue/queue.h
@@ -56,102 +56,102 @@ namespace sl
 		Container _con;
 	};
 
-	// template<class T>
-	// class less
-	// {
-	// public:
-	// 	bool operator()(const T& x, const T& y)
-	// 	{
-	// 		return x < y;
-	// 	}
-	// };
+  template<class T>
+	class less
+	{
+	public:
+		bool operator()(const T& x, const T& y)
+		{
+			return x < y;
+		}
+	};
 
-	// template<class T>
-	// class greater
-	// {
-	// public:
-	// 	bool operator()(const T& x, const T& y)
-	// 	{
-	// 		return x > y;
-	// 	}
-	// };
+	template<class T>
+	class greater
+	{
+	public:
+		bool operator()(const T& x, const T& y)
+		{
+			return x > y;
+		}
+	};
 
-	// template<class T, class Container = vector<T>, class Compare = less<T>>
-	// class priority_queue
-	// {
-	// public:
-	// 	void adjust_up(size_t child)
-	// 	{
-	// 		Compare com;
-	// 		size_t parent = (child - 1) / 2;
-	// 		while (child > 0)
-	// 		{
-	// 			if (com(_con[parent], _con[child]))
-	// 			{
-	// 				swap(_con[child], _con[parent]);
-	// 				child = parent;
-	// 				parent = (child - 1) / 2;
-	// 			}
-	// 			else
-	// 			{
-	// 				break;
-	// 			}
-	// 		}
-	// 	}
+	template<class T, class Container = vector<T>, class Compare = less<T>>
+	class priority_queue
+	{
+	public:
+		void adjust_up(size_t child)
+		{
+			Compare com;
+			size_t parent = (child - 1) / 2;
+			while (child > 0)
+			{
+				if (com(_con[parent], _con[child]))
+				{
+					swap(_con[child], _con[parent]);
+					child = parent;
+					parent = (child - 1) / 2;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
 
-	// 	void push(const T& x)
-	// 	{
-	// 		_con.push_back(x);
-	// 		adjust_up(_con.size() - 1);
-	// 	}
+		void push(const T& x)
+		{
+			_con.push_back(x);
+			adjust_up(_con.size() - 1);
+		}
 
-	// 	void adjust_down(size_t parent)
-	// 	{
-	// 		Compare com;
-	// 		size_t child = parent * 2 + 1;
-	// 		while (child < _con.size())
-	// 		{
-	// 			if (child + 1 < _con.size() && com(_con[child], _con[child + 1]))
-	// 			{
-	// 				++child;
-	// 			}
+		void adjust_down(size_t parent)
+		{
+			Compare com;
+			size_t child = parent * 2 + 1;
+			while (child < _con.size())
+			{
+				if (child + 1 < _con.size() && com(_con[child], _con[child + 1]))
+				{
+					++child;
+				}
 
-	// 			if(com(_con[parent], _con[child]))
-	// 			{
-	// 				swap(_con[child], _con[parent]);
-	// 				parent = child;
-	// 				child = parent * 2 + 1;
-	// 			}
-	// 			else
-	// 			{
-	// 				break;
-	// 			}
-	// 		}
-	// 	}
+				if(com(_con[parent], _con[child]))
+				{
+					swap(_con[child], _con[parent]);
+					parent = child;
+					child = parent * 2 + 1;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
 
-	// 	void pop()
-	// 	{
-	// 		swap(_con[0], _con[_con.size() - 1]);
-	// 		_con.pop_back();
-	// 		adjust_down(0);
-	// 	}
+		void pop()
+		{
+			swap(_con[0], _con[_con.size() - 1]);
+			_con.pop_back();
+			adjust_down(0);
+		}
 
-	// 	bool empty()
-	// 	{
-	// 		return _con.empty();
-	// 	}
+		bool empty()
+		{
+			return _con.empty();
+		}
 
-	// 	size_t size()
-	// 	{
-	// 		return _con.size();
-	// 	}
+		size_t size()
+		{
+			return _con.size();
+		}
 
-	// 	const T& top() const
-	// 	{
-	// 		return _con[0];
-	// 	}
+		const T& top() const
+		{
+			return _con[0];
+		}
 
-	// private:
-	// 	Container _con;
-	// };
+	private:
+		Container _con;
+	};
 }
