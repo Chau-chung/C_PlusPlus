5a3aaabc608be60727ddcb77499d87632fc207c8
Chau-chung <chauchung0222@gmail.com>
2025-10-22 23:08:29 +0800

    BinarySearchTree

---
+++ BinarySearchTree/BinarySearchTree.h
@@ -0,0 +1,84 @@
+﻿#pragma once
+
+template <class K>
+// struct BinarySearchTreeNode
+struct BSTreeNode
+{
+	BSTreeNode<K>* _left;
+	BSTreeNode<K>* _right;
+	K _key;
+
+	BSTreeNode(const K& key)
+		: _left(nullptr)
+		, _right(nullptr)
+		, _key(key)
+	{}
+};
+
+template <class K>
+class BSTree
+{
+	typedef BSTreeNode<K> Node;
+public:
+	bool Insert(const K& key)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(key);
+			return true;
+		}
+
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (key < cur->_key)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else if (key > cur->_key)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else
+			{
+				return false; // Key already exists
+			}
+		}
+
+		if (key < parent->_key)
+		{
+			parent->_left = new Node(key);
+		}
+		else
+		{
+			parent->_right = new Node(key);
+		}
+
+		return true;
+	}
+
+	void Erase(const K& key)
+	{
+		
+	}
+
+	void InOrder()
+	{
+		_InOrder(_root);
+	}
+
+private:
+	void _InOrder(Node* root)
+	{
+		if (root == nullptr) return;
+
+		_InOrder(root->_left);
+		std::cout << root->_key << " ";
+		_InOrder(root->_right);
+	}
+private:
+	Node* _root = nullptr;
+};
